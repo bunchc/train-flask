bfa498b824f79c5517f99f24ee80b49ec559fea3
bunchc <bunchc@gmail.com>
2017-12-09 14:19:20 -0600

    un-fixed init

--- train_controller/trainapi/models/power.py
+++ train_controller/trainapi/models/power.py
@@ -7,13 +7,15 @@ class Power(io):
     # Define the GPIO pin controlling power relay
     power_pin = 24
 
+    # Initialize GPIO, and ensure power is off
+    io.setmode(io.BCM)
+    io.setup(power_pin, io.OUT)
+    io.output(power_pin, False)
+
 
     def __init__(self, **kwargs):
         super(Power, self).__init__(**kwargs)
-        # Initialize GPIO, and ensure power is off
-        io.setmode(io.BCM)
-        io.setup(power_pin, io.OUT)
-        io.output(power_pin, False)
+
 
     def status(self):
         power_status = io.input(power_pin)
