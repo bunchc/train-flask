136821bdcb2a4d221b45c60946b73aab13337b72
bunchc <bunchc@gmail.com>
2017-12-13 15:52:51 -0600

    cleanup

--- train_controller/trainapi/api/views.py
+++ train_controller/trainapi/api/views.py
@@ -6,9 +6,6 @@ from trainapi.api.resources import (
     UserList,
     PowerControl,
     TrainControl,
-#    TrainStart,
-#    TrainStop,
-#    TrainSpeed,
 )
 
 
@@ -20,6 +17,3 @@ api.add_resource(UserResource, '/users/<int:user_id>')
 api.add_resource(UserList, '/users')
 api.add_resource(PowerControl, '/power/<pwr_ctrl>')
 api.add_resource(TrainControl, '/train')
-#api.add_resource(TrainStart, '/trian/start/<train_ctrl>')
-#api.add_resource(TrainStop, '/trian/stop/<train_ctrl>')
-#api.add_resource(TrainSpeed, '/trian/speed/<train_ctrl>')
